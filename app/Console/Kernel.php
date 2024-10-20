8c097addb4ebed9280f08874916532e6d75d2c08
bacDev <bacb4tvk@gmail.com>
2024-10-20 16:42:16 +0700

    Add test cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('reply:review')->everyFiveMinutes()->withoutOverlapping();
+        // $schedule->command('cron:test')->everyMinute()->withoutOverlapping();
     }
 
     /**
